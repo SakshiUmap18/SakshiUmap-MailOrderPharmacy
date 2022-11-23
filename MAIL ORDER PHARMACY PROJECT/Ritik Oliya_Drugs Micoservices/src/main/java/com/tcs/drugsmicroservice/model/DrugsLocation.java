5504e89404b24db431149a60142a5b33a9e5760a
Ritik393 <80672683+Ritik393@users.noreply.github.com>
2022-11-23 18:14:11 +0530

    Add files via upload

---
+++ MAIL ORDER PHARMACY PROJECT/Ritik Oliya_Drugs Micoservices/src/main/java/com/tcs/drugsmicroservice/model/DrugsLocation.java
@@ -0,0 +1,28 @@
+package com.cognizant.drugsmicroservice.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DrugsLocation {
+	@Id
+    private String serialId;
+    private String location;
+    private int quantity;
+    private String drugId;
+	
+}
