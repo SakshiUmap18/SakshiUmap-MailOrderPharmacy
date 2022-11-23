f00a857032beefc6345b0e413d7a555ee9e2e78b
Basabdutta Pal <117887887+basabdutt@users.noreply.github.com>
2022-11-23 17:25:15 +0530

    Add files via upload

---
+++ MAIL ORDER PHARMACY PROJECT/Basabdutta-Pal-authentication-microservice/src/main/java/com/tcs/authenticationmicroservice/AuthenticationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.authenticationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationMicroserviceApplication.class, args);
+	}
+
+}
